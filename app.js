35081f3e6da3b4812c34e9890bbf7d4526a4593a
Claire Matlak (Bhargavi) <clairematlak@gmail.com>
2022-12-10 19:35:38 -0500

    add color button, connect to js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 // grab DOM elements
+const nameDisplay = document.getElementById('name-display');
+const nameInput = document.getElementById('name-input');
+const button = document.getElementById('update-button');
+const nameTag = document.getElementById('nametag');
+const colorButton = document.getElementById('color-button');
+const colorInput = document.getElementById('color-input');
 
 // set event listeners
+button.addEventListener('click', ()=>{
+    console.log(nameDisplay);
+    nameDisplay.textContent = nameInput.value;
+    /*nameDisplay.style.color = 'pink';
+    nameTag.style.backgroundColor = 'plum';*/
+});
+
+colorButton.addEventListener('click', ()=>{
+    nameTag.style.backgroundColor = colorInput.value;
+});
+
